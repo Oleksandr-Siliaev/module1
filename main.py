d5200628afa0517139f8ec3dfcf7d1e93f253cbd
Oleksandr <tortikteo@gmail.com>
2023-03-28 10:14:20 +0300

    def count_sentences

--- main.py
+++ main.py
@@ -6,11 +6,21 @@ def count_words(filename):
         words = text.split()
         return len(words)
 
+def count_sentences(filename):
+    with open(filename, 'r') as file:
+        text = file.read()
+        count = 0
+        for char in text:
+            if char in ['.', '!', '?']:
+                count += 1
+        return count
 
 def main():
     filename = input("Введіть назву файлу: ")
     num_words = count_words(filename)
+    num_sentences = count_sentences(filename)
     print("Кількість слів: ", num_words)
+    print("Кількість речень: ", num_sentences)
 
 if __name__ == '__main__':
     main()
