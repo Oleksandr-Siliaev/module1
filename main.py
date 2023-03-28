59cfb08b0c6a90fc2df632450b33697fedff46f9
Oleksandr <tortikteo@gmail.com>
2023-03-28 10:15:33 +0300

    unittest

--- main.py
+++ main.py
@@ -24,4 +24,24 @@ def main():
 
 if __name__ == '__main__':
     main()
+import unittest
+import os
 
+class TestCountFunctions(unittest.TestCase):
+
+    def test_count_words(self):
+        filename = 'test_file.txt'
+        with open(filename, 'w') as file:
+            file.write("Це тестовий файл. В ньому повинно бути дев'ять слів.")
+        self.assertEqual(count_words(filename), 9)
+        os.remove(filename)
+
+    def test_count_sentences(self):
+        filename = 'test_file.txt'
+        with open(filename, 'w') as file:
+            file.write("Це тестовий файл. В ньому повинно бути два речення.")
+        self.assertEqual(count_sentences(filename), 2)
+        os.remove(filename)
+
+if __name__ == '__main__':
+    unittest.main()
